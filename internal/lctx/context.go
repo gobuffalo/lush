a58652c01d0d35860eddef0722e99ed6e3afb4b4
Mark Bates <mark@markbates.com>
2019-05-24 17:30:08 -0400

    removed some old code

--- internal/lctx/context.go
+++ internal/lctx/context.go
@@ -13,15 +13,6 @@ type Context interface {
 	Set(key string, value interface{})
 }
 
-//
-// type HelperContext interface {
-// 	Context
-// 	Block() (interface{}, error)
-// 	BlockWith(Context) (interface{}, error)
-// 	HasBlock() bool
-// 	Exec((c *Context)) (interface{}, error)
-// }
-
 type upable interface {
 	setup(key string, value interface{})
 }

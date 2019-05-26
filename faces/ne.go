3303a9b03ceeeb6ee167fa55cd03ced3986312fa
Mark Bates <mark@markbates.com>
2019-05-26 00:25:26 -0400

    Update faces/ne.go
    Co-Authored-By: fixmie[bot] <fixmie[bot]@users.noreply.github.com>

--- faces/ne.go
+++ faces/ne.go
@@ -1,6 +1,6 @@
 package faces
 
-// Equal defines an interface to support the
+// NotEqual defines an interface to support the
 // checking "equality" of one type to an another.
 type NotEqual interface {
 	NotEqual(b interface{}) (bool, error)

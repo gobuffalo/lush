3272b890005a93257d07cd38a7cbce8d8f02776d
Mark Bates <mark@markbates.com>
2019-05-27 00:19:25 -0400

    removed comments

--- compile/golang/block.go
+++ compile/golang/block.go
@@ -13,17 +13,6 @@ func (c Compiler) astBlock(b *ast.Block) error {
 
 	if len(b.Statements) > 0 {
 		fmt.Fprintln(c)
-		// x := b.Statements.GoString()
-		// x = strings.TrimSpace(x)
-		// scan := bufio.NewScanner(strings.NewReader(x))
-		// for scan.Scan() {
-		// 	s := scan.Text()
-		// 	if len(strings.TrimSpace(s)) == 0 {
-		// 		fmt.Fprintf(c, "\n")
-		// 		continue
-		// 	}
-		// 	fmt.Fprintf(c, fmt.Sprintf("\t%s\n", s))
-		// }
 	}
 	return c.astStatements(b.Statements)
 }

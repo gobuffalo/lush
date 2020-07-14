935698892252a4e28f2ce02381c00b18b9a52bc2
Mark Bates <metabates@apple.com>
2020-07-14 10:29:33 -0400

    fix

--- internal/parser/meta.go
+++ internal/parser/meta.go
@@ -1,15 +1,18 @@
 package parser
 
 import (
+	"fmt"
+
 	"github.com/gobuffalo/lush/ast"
 )
 
 func meta(c *current) ast.Meta {
+	fmt.Printf(">>>TODO internal/parser/meta.go:10: c (%T): %v \n", c, c)
 	var fn string
-	f, ok := c.globalStore["filename"]
-	if ok {
-		fn, _ = f.(string)
-	}
+	// f, ok := c.globalStore["filename"]
+	// if ok {
+	// 	fn, _ = f.(string)
+	// }
 	m := ast.Meta{
 		Filename: fn,
 		Line:     c.pos.line,
@@ -21,6 +24,7 @@ func meta(c *current) ast.Meta {
 }
 
 func MetaOption(p *parser) Option {
-	p.cur.globalStore["filename"] = p.filename
+	fmt.Printf(">>>TODO internal/parser/meta.go:24: p (%T): %v \n", p, p)
+	// p.cur.globalStore["filename"] = p.filename
 	return nil
 }
